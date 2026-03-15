a9bd3af7d97977ba6b8b37defa608e3833000b35
MiguelRuizSanchez <mruisan2202@g.educaand.es>
2026-03-15 19:47:05 +0100

    actualizacion backend

---
+++ ConcesionarioAPI/ConcesionarioAPI/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ConcesionarioAPI.Models;
+
+namespace ConcesionarioAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Vehiculo> Vehiculos { get; set; }
+    }
+}
